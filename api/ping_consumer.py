04f0308f98c48ab0c2f43cdbf4c8bc925bc97946
hieucao <hieucaohd@gmail.com>
2022-09-26 20:46:50 +0700

    update 1

--- api/ping_consumer.py
+++ api/ping_consumer.py
@@ -9,54 +9,60 @@ dir = dirname(abspath(__file__))
 
 
 def start_consumer():
-    print("enter consumer success")
-    consumer = None
-
-    try:
-        conf = ccloud_lib.read_ccloud_config(join(dir, '..', 'config', 'python.config'))
-        consumer_conf = ccloud_lib.pop_schema_registry_params_from_config(conf)
-        consumer_conf['group.id'] = 'python_example_group_1'
-        consumer_conf['auto.offset.reset'] = 'earliest'
-        consumer = Consumer(consumer_conf)
-    except Exception as er:
-        print("{}".format(er))
-        return
-
-    print("Constructor consumer success")
-
-    # Subscribe to topic
-    consumer.subscribe(['test1'])
-
-    # Process messages
-    total_count = 0
-    try:
-        while True:
-            msg = consumer.poll(1.0)
-            if msg is None:
-                # No message available within timeout.
-                # Initial message consumption may take up to
-                # `session.timeout.ms` for the consumer group to
-                # rebalance and start consuming
-                print("Waiting for message or event/error in poll()")
-                continue
-            elif msg.error():
-                print('error: {}'.format(msg.error()))
-            else:
-                # Check for Kafka message
-                record_key = msg.key()
-                record_value = msg.value()
-                data = json.loads(record_value)
-                count = data['count']
-                total_count += count
-                print("Consumed record with key {} and value {}, \
-                      and updated total count to {}"
-                      .format(record_key, record_value, total_count))
-    except KeyboardInterrupt:
-        print("consumer stop by key")
-    finally:
-        # Leave group and commit final offsets
-        print("consumer close")
-        consumer.close()
+    # print("enter consumer success")
+    # consumer = None
+    #
+    # try:
+    #     conf = ccloud_lib.read_ccloud_config(join(dir, '..', 'config', 'python.config'))
+    #     consumer_conf = ccloud_lib.pop_schema_registry_params_from_config(conf)
+    #     consumer_conf['group.id'] = 'python_example_group_1'
+    #     consumer_conf['auto.offset.reset'] = 'earliest'
+    #     consumer = Consumer(consumer_conf)
+    # except Exception as er:
+    #     print("{}".format(er))
+    #     return
+    #
+    # print("Constructor consumer success")
+    #
+    # # Subscribe to topic
+    # consumer.subscribe(['test1'])
+    #
+    # # Process messages
+    # total_count = 0
+    # try:
+    #     while True:
+    #         msg = consumer.poll(1.0)
+    #         if msg is None:
+    #             # No message available within timeout.
+    #             # Initial message consumption may take up to
+    #             # `session.timeout.ms` for the consumer group to
+    #             # rebalance and start consuming
+    #             print("Waiting for message or event/error in poll()")
+    #             continue
+    #         elif msg.error():
+    #             print('error: {}'.format(msg.error()))
+    #         else:
+    #             # Check for Kafka message
+    #             record_key = msg.key()
+    #             record_value = msg.value()
+    #             data = json.loads(record_value)
+    #             count = data['count']
+    #             total_count += count
+    #             print("Consumed record with key {} and value {}, \
+    #                   and updated total count to {}"
+    #                   .format(record_key, record_value, total_count))
+    # except KeyboardInterrupt:
+    #     print("consumer stop by key")
+    # finally:
+    #     # Leave group and commit final offsets
+    #     print("consumer close")
+    #     consumer.close()
+    count = 0
+    while True:
+        if count == 100000:
+            return
+        print(f"Hello {count}")
+        count += 1
 
 
 class handler(BaseHTTPRequestHandler):
@@ -67,6 +73,7 @@ class handler(BaseHTTPRequestHandler):
         self.end_headers()
         x = threading.Thread(target=start_consumer)
         x.start()
+        print("end response")
         return
 
 
